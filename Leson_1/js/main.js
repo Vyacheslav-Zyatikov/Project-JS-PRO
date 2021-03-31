a9105ededbce6a597ed61ff6ececc9c0be5fd80e
Vyacheslav Zyatikov <vyacheslav.zyatikov@yandex.com>
2021-03-31 11:35:34 +0300

    Leson-1

--- Leson_1/js/main.js
+++ Leson_1/js/main.js
@@ -3,19 +3,20 @@ const products = [
     {id: 2, title: 'Mouse', price: 20},
     {id: 3, title: 'Keyboard', price: 200},
     {id: 4, title: 'Gamepad', price: 50},
+    {id: 5, title: 'Game', price: 70},
+    {id: 6, title: 'Cable', price: 5},
 ];
 //Функция для формирования верстки каждого товара
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
+const renderProduct = (product, img='https://place-hold.it/200x150') => {
+    return `<div class="product-item grid">
+                <img src="${img}" alt="photo">
+                <h3>${product.title}</h3>
+                <p>$ ${product.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');
 };
 
 renderPage(products);
