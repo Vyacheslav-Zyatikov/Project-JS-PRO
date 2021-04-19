a08ddf6c09c9fc96d93a53fecec20a1ecfbcbb19
Vyacheslav Zyatikov <vyacheslav.zyatikov@yandex.com>
2021-04-19 11:18:22 +0300

    19.01

--- Leson_5/js/ErrorComponent.js
+++ Leson_5/js/ErrorComponent.js
@@ -2,7 +2,7 @@ Vue.component('error', {
     template:
         `
         <div class="grid">
-            <h1 v-if="$parent.products.length === 0">Нет связи с сервером</h1>
+            <h1 v-if="$root.error === true">Нет связи с сервером</h1>
         </div>
         `
 })
